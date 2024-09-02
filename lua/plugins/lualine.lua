41fd16f682f1a8be7b38a01b77bed54afc107224
Eric Xiao <ericxiao0000@gmail.com>
2024-09-02 16:26:35 -0700

    feat: simplify lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,12 +3,16 @@ return {
 		"nvim-lualine/lualine.nvim",
 		opts = {
 			sections = {
+				lualine_a = {},
+				lualine_b = { "branch", "diagnostics" },
 				lualine_c = {
 					{
 						"filename",
 						path = 1,
 					},
 				},
+				lualine_x = {},
+				lualine_y = {},
 			},
 		},
 		dependencies = { "nvim-tree/nvim-web-devicons" },
