b8c8964dac45c04deeca0df1b7ddf6d30acae609
Eric Xiao <ericxiao0000@gmail.com>
2024-09-08 22:32:21 -0700

    update oil config

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -10,21 +10,36 @@ return {
 				show_hidden = true,
 			},
 			keymaps = {
-				["<C-s>"] = function()
-					local split_file = require("oil").get_cursor_entry().parsed_name
-					local dir = require("oil").get_current_dir(0)
-					require("oil").close()
-					vim.cmd("vs")
-					vim.cmd("e " .. dir .. split_file)
-				end,
-				["gy"] = function()
-					local file = require("oil").get_cursor_entry().parsed_name
-					local dir = require("oil").get_current_dir(0)
-					vim.fn.setreg("+", dir .. file)
-				end,
-				[")"] = function()
-					require("oil").select()
-				end,
+				["<C-s>"] = {
+					function()
+						local split_file = require("oil").get_cursor_entry().parsed_name
+						local dir = require("oil").get_current_dir(0)
+						require("oil").close()
+						vim.cmd("vs")
+						vim.cmd("e " .. dir .. split_file)
+					end,
+					desc = "Open File in Split and Reopen Previous Buffer",
+				},
+				["gy"] = {
+					function()
+						local file = require("oil").get_cursor_entry().parsed_name
+						local dir = require("oil").get_current_dir(0)
+						vim.fn.setreg("+", dir .. file)
+					end,
+					desc = "Copy Entry Name to System Clipboard",
+				},
+				[")"] = {
+					function()
+						require("oil").select()
+					end,
+					desc = "Select Current Entry",
+				},
+				["<leader><c-o>"] = {
+					function()
+						vim.cmd("silent exec '!open " .. require("oil").get_current_dir() .. "'")
+					end,
+					desc = "Open CWD with Default System File Explorer",
+				},
 			},
 		},
 		dependencies = {
