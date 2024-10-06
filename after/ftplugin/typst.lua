b4c5e4c5740cfb24ede1ddd06346f59b56f22dd8
Eric Xiao <ericxiao0000@gmail.com>
2024-10-06 11:36:17 -0700

    feat: add typst line wrap, fix issue with harpoon filetype options

---
+++ after/ftplugin/typst.lua
@@ -0,0 +1,8 @@
+vim.opt_local.wrap = true
+
+local remap_options = { silent = true, buffer = 0, noremap = true }
+
+vim.keymap.set("n", "j", "gj", remap_options)
+vim.keymap.set("n", "k", "gk", remap_options)
+vim.keymap.set("n", "0", "g0", remap_options)
+vim.keymap.set("n", "$", "g$", remap_options)
--- init.lua
+++ init.lua
@@ -7,3 +7,12 @@ vim.api.nvim_create_autocmd("VimEnter", {
 		print("Welcome to Neovim !")
 	end,
 })
+
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+	desc = "Source ftplugin/$1.lua to override Issue #626",
+	group = vim.api.nvim_create_augroup("Harpoon_Optlocal", { clear = true }),
+	callback = function()
+		local ft = vim.bo.filetype
+		vim.cmd("silent! source ~/.config/nvim/after/ftplugin/" .. ft .. ".lua")
+	end,
+})
