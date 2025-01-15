9fa04a339799ff8c7a6dbff76598b8a0cd86bf2e
Eric Xiao <ericxiao0000@gmail.com>
2025-01-14 22:58:30 -0800

    feat: update lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -174,46 +174,16 @@ return {
 			--  - settings (table): Override the default settings passed when initializing the server.
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
-				arduino_language_server = {
-					cmd = {
-						"arduino-language-server",
-						"-clangd",
-						"/Users/avah/.local/share/nvim/mason/bin/clangd",
-						"-cli",
-						"/opt/homebrew/bin/arduino-cli",
-						"-cli-config",
-						"/Users/avah/.arduinoIDE/arduino-cli.yaml",
-						"-fqbn",
-						"arduino:avr:uno",
-					},
-				},
-				java_language_server = {
-					cmd = {
-						vim.fn.expand("~/.local/share/nvim/mason/bin/java-language-server"),
-					},
-				},
 				tinymist = {
 					settings = {
+						formatterMode = "typstfmt",
 						exportPdf = "onSave",
-						outputPath = "$root/$dir/$name",
+						outputPath = "$root/.typst/$dir/$name",
 					},
 				},
 				clangd = {},
-				-- gopls = {},
-				-- pyright = {},
-				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-				--
-				-- Some languages (like typescript) have entire language plugins that can be useful:
-				--    https://github.com/pmizio/typescript-tools.nvim
-				--
-				-- But for many setups, the LSP (`tsserver`) will work just fine
-				-- tsserver = {},
-
 				lua_ls = {
-					-- cmd = {...},
-					-- filetypes = { ...},
-					-- capabilities = {},
 					settings = {
 						Lua = {
 							completion = {
@@ -230,8 +200,6 @@ return {
 			--  To check the current status of installed tools and/or manually install
 			--  other tools, you can run
 			--    :Mason
-			--
-			--  You can press `g?` for help in this menu.
 			require("mason").setup()
 
 			-- You can add other tools here that you want Mason to install
@@ -245,12 +213,13 @@ return {
 			require("mason-lspconfig").setup({
 				handlers = {
 					function(server_name)
-						local server = servers[server_name] or {}
+						local server_config = servers[server_name] or {}
 						-- This handles overriding only values explicitly passed
 						-- by the server configuration above. Useful when disabling
 						-- certain features of an LSP (for example, turning off formatting for tsserver)
-						server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
-						require("lspconfig")[server_name].setup(server)
+						server_config.capabilities =
+							vim.tbl_deep_extend("force", {}, capabilities, server_config.capabilities or {})
+						require("lspconfig")[server_name].setup(server_config)
 					end,
 				},
 			})
