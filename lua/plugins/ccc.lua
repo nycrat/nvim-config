37133cdd0897b24952c83872f3c6d1524cce9142
Eric Xiao <ericxiao0000@gmail.com>
2025-05-17 01:15:56 -0700

    ccc.nvim picker settings

--- lua/plugins/ccc.lua
+++ lua/plugins/ccc.lua
@@ -1,13 +1,25 @@
 return {
 	{
 		"uga-rosa/ccc.nvim",
-		opts = {
-			highlighter = {
-				auto_enable = true,
+		config = function()
+			local ccc = require("ccc")
+			ccc.setup({
+				highlighter = {
+					auto_enable = true,
+					lsp = true,
+				},
 				lsp = true,
-			},
-			lsp = true,
-			save_on_quit = true,
-		},
+				save_on_quit = true,
+				inputs = {
+					ccc.input.rgb,
+					ccc.input.oklch,
+				},
+				outputs = {
+					ccc.output.hex,
+					ccc.output.css_rgb,
+					ccc.output.css_oklch,
+				},
+			})
+		end,
 	},
 }
