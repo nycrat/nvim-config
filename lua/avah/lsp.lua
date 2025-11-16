29cbfc70f5657d78b18684d8129ac0ff4e7a07bc
Eric Xiao <ericxiao0000@gmail.com>
2025-11-16 13:24:23 -0800

    Add tinymist lsp

--- lua/avah/lsp.lua
+++ lua/avah/lsp.lua
@@ -1,4 +1,4 @@
-vim.lsp.enable { "lua_ls", "gopls", "astro", "ts_ls", "cssls", "pylsp", "html", "clangd", "tailwindcss" }
+vim.lsp.enable { "lua_ls", "gopls", "astro", "ts_ls", "cssls", "pylsp", "html", "clangd", "tailwindcss", "tinymist" }
 
 vim.lsp.config("lua_ls", {
   settings = {
