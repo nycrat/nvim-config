761a49e4ce16fa23bc7918ca3bd56c0757d3465d
Eric Xiao <ericxiao0000@gmail.com>
2025-11-24 20:43:18 -0800

    Fix cmp mappings

--- lua/avah/cmp.lua
+++ lua/avah/cmp.lua
@@ -1,13 +1,13 @@
 require "mini.completion".setup {
   delay = { completion = 5, info = 5, signature = 5 },
   mappings = {
-    force_twostep = "<c-n>",
+    force_twostep = "<c-l>",
   }
 }
 
 -- Disables using Enter to complete
 vim.keymap.set("i", "<cr>", function()
-  if vim.fn.pumvisible() then
+  if vim.fn.pumvisible() ~= 0 then
     return vim.keycode "<c-e><cr>"
   else
     return vim.keycode "<cr>"
