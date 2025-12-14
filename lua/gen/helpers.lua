57f356b0a35b3f71e5004aeb6c9cb1d4c2ff9267
Eric Xiao <ericxiao0000@gmail.com>
2025-12-14 12:28:54 -0800

    Fix formatting multiple times

--- lua/gen/helpers.lua
+++ lua/gen/helpers.lua
@@ -9,19 +9,20 @@ M.specify_formatter = function(filetype, server)
 		group = vim.api.nvim_create_augroup("Auto_Formatting", { clear = true }),
 		callback = function(ev)
 			local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+
+			local target_formatter = special_formatting[vim.bo.filetype]
+
+			if target_formatter ~= nil and target_formatter ~= client.name then
+				return
+			end
+
 			if client:supports_method("textDocument/formatting") then
 				vim.api.nvim_create_autocmd("BufWritePre", {
 					buffer = ev.buf,
 					callback = function()
 						if vim.g.autoformat then
 							vim.lsp.buf.format({
-								async = true,
-								filter = function(c)
-									if special_formatting[vim.bo.filetype] ~= nil then
-										return special_formatting[vim.bo.filetype] == c.name
-									end
-									return true
-								end
+								async = true
 							})
 						end
 					end,
